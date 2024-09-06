e929b86748ae9568ac87c903ff97454e7108ba9a
Garaisourav12 <garaisourav12@gmail.com>
2024-09-06 10:05:20 +0530

    v12

--- index.js
+++ index.js
@@ -4,71 +4,76 @@ const fs = require("fs").promises;
 const { existsSync } = require("fs");
 const path = require("path");
 const cls = require("cli-color"); // Use cls for color formatting
-const inquirer = require("inquirer");
 
-inquirer
-	.prompt([
-		{
-			type: "input",
-			name: "projectName",
-			message: "Enter project name:",
-			default: "", // Default to current directory
-		},
-	])
-	.then(async (answers) => {
-		let projectName = answers.projectName;
-		let projectDir = projectName;
+(async () => {
+	const inquirer = (await import("inquirer")).default;
 
-		// If project name is empty
-		if (!projectName) {
-			throw new Error("Project name is required!");
-		}
-		// If project name is ".", use current directory name
-		if (projectName === ".") {
-			projectName = path.basename(process.cwd()); // Use current directory name
-			projectDir = "."; // Set to current directory
-		}
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				name: "projectName",
+				message: "Enter project name:",
+				default: "", // Default to current directory
+			},
+		])
+		.then(async (answers) => {
+			let projectName = answers.projectName;
+			let projectDir = projectName;
 
-		// If project name is not provided, throw an error
-		if (!projectName) {
-			throw new Error("Project name is required!");
-		}
-
-		// Create the project directory if it doesn't exist
-		if (projectDir !== "." && !existsSync(projectDir)) {
-			await fs.mkdir(projectDir);
-		}
-
-		const sourceDir = path.join(__dirname, "create-express-app");
-
-		try {
-			await copyDirectory(sourceDir, projectDir);
-			console.log(
-				cls.green(`Project created successfully in ${projectDir}!`)
-			);
-
-			// Instructions
-			console.log(cls.green(`\nTo setup the project, run:`));
-			if (projectDir === ".") {
-				console.log(cls.green(`cd ${projectName}`));
+			// If project name is empty
+			if (!projectName) {
+				throw new Error("Project name is required!");
 			}
-			console.log(cls.green(`npm install`));
-			console.log(cls.green(`\nRun your application with:`));
-			console.log(cls.green(`npm start - for production`));
-			console.log(cls.green(`npm run dev - for development`));
+			// If project name is ".", use current directory name
+			if (projectName === ".") {
+				projectName = path.basename(process.cwd()); // Use current directory name
+				projectDir = "."; // Set to current directory
+			}
 
-			// Modify package.json and package-lock.json
-			await updatePackageFiles(projectDir, projectName);
-			// Update .gitignore
-			await updateGitIgnore(projectDir);
-		} catch (err) {
-			console.error(cls.red(`Error while copying files: ${err.message}`));
-			process.exit(1);
-		}
-	})
-	.catch((err) => {
-		console.error(cls.red(`Error during prompt: ${err.message}`));
-	});
+			// If project name is not provided, throw an error
+			if (!projectName) {
+				throw new Error("Project name is required!");
+			}
+
+			// Create the project directory if it doesn't exist
+			if (projectDir !== "." && !existsSync(projectDir)) {
+				await fs.mkdir(projectDir);
+			}
+
+			const sourceDir = path.join(__dirname, "create-express-app");
+
+			try {
+				await copyDirectory(sourceDir, projectDir);
+				console.log(
+					cls.green(`Project created successfully in ${projectDir}!`)
+				);
+
+				// Instructions
+				console.log(cls.green(`\nTo setup the project, run:`));
+				if (projectDir === ".") {
+					console.log(cls.green(`cd ${projectName}`));
+				}
+				console.log(cls.green(`npm install`));
+				console.log(cls.green(`\nRun your application with:`));
+				console.log(cls.green(`npm start - for production`));
+				console.log(cls.green(`npm run dev - for development`));
+
+				// Modify package.json and package-lock.json
+				await updatePackageFiles(projectDir, projectName);
+				// Update .gitignore
+				await updateGitIgnore(projectDir);
+			} catch (err) {
+				console.error(
+					cls.red(`Error while copying files: ${err.message}`)
+				);
+				process.exit(1);
+			}
+		})
+		.catch((err) => {
+			console.error(cls.red(`Error during prompt: ${err.message}`));
+		});
+})();
 
 // Function to copy directory recursively
 async function copyDirectory(source, destination) {
